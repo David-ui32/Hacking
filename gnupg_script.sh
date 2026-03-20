24674f84f2b8b6de6dbebdcd98d52b56350cfeab
David-ui32 <cristo123456xx@gmail.com>
2026-03-20 18:02:20 +0000

    libs and key generation

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,4 @@
 uname -a #Daba informacion sobre el ordenador 
 which gpg #Daba la ruta del programa gpg
 gpg --version #nos da la version e informacion adicional 
+gpg --full-generate-key #genera una llave publica y privada 
