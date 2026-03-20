bd0824c06df4b1629619fc5e09b332f77d818246
David-ui32 <cristo123456xx@gmail.com>
2026-03-20 18:07:18 +0000

    llave publica

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,5 @@ uname -a #Daba informacion sobre el ordenador
 which gpg #Daba la ruta del programa gpg
 gpg --version #nos da la version e informacion adicional 
 gpg --full-generate-key #genera una llave publica y privada 
+pgp --armor --export #exporta la llave publica en formato
+gpg --list-keys #muestra las llaves publicas que tenemos
